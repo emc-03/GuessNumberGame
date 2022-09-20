521875435f91697003a8dcff3411a158442a5b74
emc-03 <74474466+emc-03@users.noreply.github.com>
2022-09-20 14:52:46 -0600

    Add files via upload

---
+++ numberGuesser/numberGuesser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Namespace
+namespace numberGuesser
+{
+    //Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            string name = "Emily Carter";
+            // START HERE // 
+            Console.WriteLine("Hello" + name );
+
+        }
+    }
+}
